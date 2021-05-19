3545793bf810a19829e14afd203d10774e3a3b81
y2sec <bae3835@gmail.com>
2021-05-19 16:05:16 +0900

    카테고리 엔티티 수정
    - @ToString 추가

--- src/main/java/xyz/y2sec/blog/category/model/Category.java
+++ src/main/java/xyz/y2sec/blog/category/model/Category.java
@@ -1,6 +1,7 @@
 package xyz.y2sec.blog.category.model;
 
 import lombok.Getter;
+import lombok.ToString;
 import xyz.y2sec.blog.post.model.Post;
 
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@ToString
 public class Category {
 
     @Id
