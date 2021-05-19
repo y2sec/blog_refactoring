f1745229c06951ca7953ac8d10ead294018d05ad
y2sec <bae3835@gmail.com>
2021-05-19 16:04:55 +0900

    카테고리 기능 구현
    - CategoryDto 추가

---
+++ src/main/java/xyz/y2sec/blog/category/dto/CategoryDto.java
@@ -0,0 +1,14 @@
+package xyz.y2sec.blog.category.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CategoryDto {
+
+    private String name;
+
+}
