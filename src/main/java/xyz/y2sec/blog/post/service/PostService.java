90b9e793bf60d9f7b59be3b77ff0987f9fa4537e
y2sec <bae3835@gmail.com>
2021-05-29 16:24:24 +0900

    게시글 Service 추가
    - PostService 인터페이스 추가
    - PostService 인터페이스 구현체 PostServiceImpl 추가
    - 전체 조회, PostId를 통한 조회, CategoryId를 통한 조회, 게시물 추가, 게시물 수정, 게시물 삭제 비즈니스 로직 구현

---
+++ src/main/java/xyz/y2sec/blog/post/service/PostService.java
@@ -0,0 +1,23 @@
+package xyz.y2sec.blog.post.service;
+
+import xyz.y2sec.blog.category.model.Category;
+import xyz.y2sec.blog.post.dto.PostDto;
+import xyz.y2sec.blog.post.model.Post;
+
+import java.util.List;
+
+public interface PostService {
+
+    List<Post> findAll();
+
+    List<Post> findByCategoryId(long categoryId);
+
+    Post findById(long postId);
+
+    long addPost(Post post);
+
+    void modifyPost(PostDto postDto, Category category, long postId);
+
+    void removePost(long postId);
+
+}
