2eb2bf19d619c0377bcf9b2858bcd0bdab29484a
y2sec <bae3835@gmail.com>
2021-06-17 19:22:19 +0900

    Comment DTO 추가

---
+++ src/main/java/xyz/y2sec/blog/comment/dto/CommentDto.java
@@ -0,0 +1,14 @@
+package xyz.y2sec.blog.comment.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentDto {
+
+    private String name;
+    private String content;
+    private String password;
+
+}
