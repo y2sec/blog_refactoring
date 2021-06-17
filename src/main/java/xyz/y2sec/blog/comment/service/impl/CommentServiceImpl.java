4c16b75be844e80a3d9ffc0a6461e834a7aac2e0
y2sec <bae3835@gmail.com>
2021-06-17 19:21:31 +0900

    CommentService 추가
    - 조회, 수정, 추가, 삭제 구현

---
+++ src/main/java/xyz/y2sec/blog/comment/service/impl/CommentServiceImpl.java
@@ -0,0 +1,71 @@
+package xyz.y2sec.blog.comment.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import xyz.y2sec.blog.comment.dto.CommentDto;
+import xyz.y2sec.blog.comment.model.Comment;
+import xyz.y2sec.blog.comment.repository.CommentRepository;
+import xyz.y2sec.blog.comment.service.CommentService;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Comment> findAll() {
+        return commentRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Comment> findByPostId(long postId) {
+        return commentRepository.findByPostId(postId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Comment findById(long commentId) {
+        Optional<Comment> findComment = commentRepository.findById(commentId);
+
+        if (findComment.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        return findComment.get();
+    }
+
+    @Override
+    public long addComment(Comment comment) {
+        Comment saveComment = commentRepository.save(comment);
+
+        return saveComment.getId();
+    }
+
+    @Override
+    public void modifyComment(CommentDto commentDto, long commentId) {
+        Optional<Comment> findComment = commentRepository.findById(commentId);
+
+        if (findComment.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        findComment.get().update(commentDto);
+    }
+
+    @Override
+    public void removeComment(long commentId) {
+        Optional<Comment> findComment = commentRepository.findById(commentId);
+
+        if (findComment.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        commentRepository.delete(findComment.get());
+    }
+}
