ad89df5e75121aca998e49cb9c48c205962c38a9
y2sec <bae3835@gmail.com>
2021-06-24 20:17:54 +0900

    Comment DTO 수정
    - 생성자 추가

--- src/main/java/xyz/y2sec/blog/comment/dto/CommentDto.java
+++ src/main/java/xyz/y2sec/blog/comment/dto/CommentDto.java
@@ -1,10 +1,12 @@
 package xyz.y2sec.blog.comment.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class CommentDto {
 
     private String name;
