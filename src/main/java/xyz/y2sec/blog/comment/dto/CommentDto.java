9b99c1b45a2b7ce1b13df2fef6c524c0f1ae3d1b
y2sec <bae3835@gmail.com>
2021-07-24 15:28:50 +0900

    #9 댓글 API 개발
    - 댓글 API 개발
    - commentService.modifyComment 파라미터 변경
    - StringUtils 추가

--- src/main/java/xyz/y2sec/blog/comment/dto/CommentDto.java
+++ src/main/java/xyz/y2sec/blog/comment/dto/CommentDto.java
@@ -2,15 +2,33 @@ package xyz.y2sec.blog.comment.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CommentDto {
 
+    public CommentDto(String name, String content, String password) {
+        this.name = name;
+        this.content = content;
+        this.password = password;
+    }
+
+    private long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
     private String content;
+
+    @NotNull
     private String password;
 
+    private long postId;
 }
